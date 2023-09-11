408670c80ee10acc404eeadac6ee40860fbb6f9d
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-09-11 15:53:29 -0400

    Create proptypes

--- src/singlePoem/SinglePoem.js
+++ src/singlePoem/SinglePoem.js
@@ -2,17 +2,19 @@ import { useParams } from "react-router-dom";
 import SinglePoemCard from "./PoemCard";
 import { useState, useEffect } from "react";
 import { getPoemById } from "../ApiCalls";
+import PropTypes from 'prop-types'
 
-function SinglePoem({poems}) {
+function SinglePoem({ poems }) {
   const [selectedPoem, setSelectedPoem] = useState({})
-  const {id} = useParams()
-
+  const { id } = useParams()
+ console.log(poems)
+//  console.log(typeof selectedPoem.poem, "poem")
   useEffect(() => {
     getPoemById(id).then(data => setSelectedPoem(data.poem))
-    }, [])
+    }, [id])
 
       
-  const poemCard = selectedPoem && (
+  const poemCard = selectedPoem ? (
     <SinglePoemCard
       id={selectedPoem.id}
       key={selectedPoem.id}
@@ -20,9 +22,18 @@ function SinglePoem({poems}) {
       author={selectedPoem.author}
       poem={selectedPoem.poem}
     />
-  )
+  ) : null
 
   return <div>{poemCard}</div>;
   }
 
 export default SinglePoem;
+
+SinglePoem.propTypes = {
+  poems: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    author: PropTypes.string,
+    poem: PropTypes.string
+  }))
+}
