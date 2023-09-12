a0ff12b6a3d0a8b107d0e986032aced9f80dec74
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-09-12 11:58:13 -0400

    Added proptypes to poems and setPoems in Form file

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,65 +1,83 @@
 import { useState } from "react";
-import './Form.css'
+import "./Form.css";
 import { postPoem } from "../ApiCalls";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-function Form({poems, setPoems}) {
-    const [title, setTitle] = useState('')
-    const [author, setAuthor] = useState('')
-    const [poem, setPoem] = useState('')
+function Form({ poems, setPoems }) {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [poem, setPoem] = useState("");
+  console.log(setPoems, "setPoems")
 
-    function submitPoem(event) {
-        const newPoem = {
-            title,
-            author,
-            poem
-        }
-        postPoem(newPoem)
-        .then(data => {
-            const updatedPoems = poems && [...poems, data]
-            setPoems(updatedPoems)
-        })
-        clearInput()
-    }
+  function submitPoem(event) {
+    const newPoem = {
+      title,
+      author,
+      poem,
+    };
+    postPoem(newPoem).then((data) => {
+      const updatedPoems = poems && [...poems, data];
+      setPoems(updatedPoems);
+    });
+    clearInput();
+  }
 
-    function clearInput() {
-        setTitle('')
-        setAuthor('')
-        setPoem('')
-    }
+  function clearInput() {
+    setTitle("");
+    setAuthor("");
+    setPoem("");
+  }
 
-    return (
-        <form className="form-container">
-            <input
-            className="form-input"
-            type="text"
-            placeholder="Title"
-            name="title"
-            value={title}
-            onChange={event => setTitle(event.target.value)}
-            />
-            <input
-            className="form-input"
-            type="text"
-            placeholder="Author"
-            name="author"
-            value={author}
-            onChange={event => setAuthor(event.target.value)}
-            />
-            <textarea rows="8" cols="50"
-            className="form-input"
-            type="text"
-            placeholder="Poem"
-            name="poem"
-            value={poem}
-            onChange={event => setPoem(event.target.value)}
-            />
-            <Link to={"/"} className="form-button" onClick={event => submitPoem(event)}>
-                Submit
-            </Link>
-        </form>
-    )
-
+  return (
+    <form className="form-container">
+      <input
+        className="form-input"
+        type="text"
+        placeholder="Title"
+        name="title"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        className="form-input"
+        type="text"
+        placeholder="Author"
+        name="author"
+        value={author}
+        onChange={(event) => setAuthor(event.target.value)}
+      />
+      <textarea
+        rows="8"
+        cols="50"
+        className="form-input"
+        type="text"
+        placeholder="Poem"
+        name="poem"
+        value={poem}
+        onChange={(event) => setPoem(event.target.value)}
+      />
+      <Link
+        to={"/"}
+        className="form-button"
+        onClick={(event) => submitPoem(event)}
+      >
+        Submit
+      </Link>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
+
+Form.propTypes = {
+  poems: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+      title: PropTypes.string,
+      author: PropTypes.string,
+      poem: PropTypes.string,
+    })
+  ),
+  setPoems: PropTypes.func
+};
