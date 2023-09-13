240e75c1a6cb9aa471e51b361cf4e40df300c189
Denzel Turambi <dturambi26@gmail.com>
2023-09-13 14:37:14 -0600

    feat: Style Error page

--- src/ErrorHandling/Error404.js
+++ src/ErrorHandling/Error404.js
@@ -1,10 +1,17 @@
+import './ErrorHandling.css'
+import { NavLink } from 'react-router-dom';
+
 function Error404() {
   return (
-    <div>
-      <p>Error 404, a digital twist, </p>
-      <p>The page you seek does not exist.</p>
-      <p>This link's journey ends right here,</p>
-      <p>return to home and have no fear!</p>
+    <div className="error-container">
+      <div className="error-box">
+        <h1 className="error-text">Oops! Page not found!</h1>
+        <h3 className="error-text">Error 404, a digital twist, </h3>
+        <h3 className="error-text">The page you seek does not exist.</h3>
+        <h3 className="error-text">This link's journey ends right here,</h3>
+        <h3 className="error-text">Return to home and have no fear!</h3>
+      </div>
+      <NavLink to="/" className="error-button">Go Back Home</NavLink>
     </div>
   )
 }
