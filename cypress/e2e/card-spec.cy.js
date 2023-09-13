9a8cfd7a7ccfcd285a79bf1912f2d5a03385b7d4
Benjamin holden <benjamin.f.holden@outlook.com>
2023-09-12 18:43:45 -0600

    Add bug fix to card cypress test

--- cypress/e2e/card-spec.cy.js
+++ cypress/e2e/card-spec.cy.js
@@ -4,7 +4,7 @@ describe('all ellements on the home page', () => {
       statusCode: 201,
       fixture: 'poems'
     }).as("poems")
-    cy.intercept('GET', 'http://localhost:3000/api/v1/poems/1', {
+    cy.intercept('GET', 'http://localhost:3000/api/v1/poems/2', {
       statusCode: 201,
       fixture: 'selectPoem'
     }).as("selectPoem")
@@ -13,11 +13,11 @@ describe('all ellements on the home page', () => {
   it('should show a selected poem page', () => {
     cy.visit('http://localhost:3001/').wait("@poems")
     .get('.poem-card').first().click().wait("@selectPoem")
-    .url().should('contain', '/1')
+    .url().should('contain', '/2')
     .get('.single-poem-card').should('have.length', 1).should('be.visible')
-    .get('.poem-title').contains('h1','Happy Day')
+    .get('.poem-title').contains('h1','Sad Day')
     .get('.poem-author').contains('h2','Written by Kapowies')
-    .get('.poem-text').contains('p', "On a happy day, the sun does shine, Its golden rays, a gift divine.The world awakens with a cheerful song, As nature's beauty dances along.")
+    .get('.poem-text').contains('p', "On a somber day, the skies are gray, As tears of rain obscure the way. A heavy heart burdened with sorrow, Longs for a brighter, hopeful tomorrow.")
     .get('.nav-buttons').should("be.visible")
   })
 })
