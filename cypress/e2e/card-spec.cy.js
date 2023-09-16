df9b7dc9f748a8502141209c4d9247cafebbfbfd
Scotty Brown <scottybrown98@gmail.com>
2023-09-16 18:20:31 -0500

    Add testing for Next Poem button

--- cypress/e2e/card-spec.cy.js
+++ cypress/e2e/card-spec.cy.js
@@ -1,4 +1,4 @@
-describe('All ellements on the home page', () => {
+describe('Single Card Spec', () => {
   beforeEach(()=>{
     cy.intercept('GET', 'https://rhythm-rizz-api-git-main-scotty-brown.vercel.app/api/v1/poems', {
       statusCode: 201,
@@ -8,9 +8,14 @@ describe('All ellements on the home page', () => {
       statusCode: 201,
       fixture: 'selectPoem'
     }).as("selectPoem")
+    cy.intercept('GET', 'https://rhythm-rizz-api-git-main-scotty-brown.vercel.app/api/v1/poems/1', {
+      statusCode: 201,
+      fixture: 'randomPoem'
+    }).as("randomPoem")
+
   })
 
-  it('should show a selected poem page', () => {
+  it('Should show a selected poem page', () => {
     cy.visit('https://rhythm-rizz-ui.vercel.app/').wait("@poems")
     .get('.poem-card').first().click().wait("@selectPoem")
     .url().should('contain', '/2')
@@ -19,5 +24,19 @@ describe('All ellements on the home page', () => {
     .get('.single-poem-author').contains('h2','Written by: Kapowies')
     .get('.single-poem-text').contains('p', "On a somber day, the skies are gray, As tears of rain obscure the way. A heavy heart burdened with sorrow, Longs for a brighter, hopeful tomorrow.")
     .get('.nav-buttons').should("be.visible")
+    .get('.random-poem').should('contain', "Next Poem")
   })
+
+  it('Should show you another poem after Next Poem is clicked', () => {
+    cy.visit('https://rhythm-rizz-ui.vercel.app/').wait("@poems")
+    .get('.poem-card').first().click().wait("@selectPoem")
+    .url().should('contain', '/2')
+    .get('.random-poem').click().wait('@randomPoem')
+    .get('.single-poem-card').should('have.length', 1).should('be.visible')
+    .get('.single-poem-title').contains('h1','Happy Day')
+    .get('.single-poem-author').contains('h2','Written by: Kapowies')
+    .get('.single-poem-text').contains('p', "On a happy day, the sun does shine, Its golden rays, a gift divine.The world awakens with a cheerful song, As nature's beauty dances along.")
+    .get('.nav-buttons').should("be.visible")
+  })
+
 })
